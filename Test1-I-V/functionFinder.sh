1641072c1fcf91ede7f0e073160ce8ee790e62da
shanahdt <daniel.shanahan@gmail.com>
2014-07-29 23:10:23 -0500

    function finder script for Test1-I-V

--- Test1-I-V/functionFinder.sh
+++ Test1-I-V/functionFinder.sh
@@ -1,14 +1,21 @@
+######OPENING SUBSTITUTIONS##########
+
+awk 'BEGIN{FS=","}{if ($1 ~ /^[0-9\.]V/) print $1"\011"$2"\011""D"; 
+else if ($1 ~ /[0-9\.]V/) print $1"\011"$2"\011""D"; 
+else if ($1 ~ /IV/) print $1"\011"$2"\011""P";
+else if ($1 ~ /vi/) print $1"\011"$2"\011""T";
+else if ($1 ~ /I[abc]/) print $1"\011"$2"\011""T";
+else if ($1 ~ /^[0-9\.][iI]"\011"/) print $1"\011"$2"\011""T";
+else print $0}' $1  > /tmp/$1.subbedOpening
 
 ############I Chords ##################
 ####  Rule 1:  Label opening I chords as Tonic
-# patt -f ../patterns/openingTonic -t "T" $1 | ditto -s "=\t="
-# awk '{if ($3 ~ /=-/) $(NF--)=""; print}' | sed 's/=- /=-	/g'
-opening_tonic1=$(pattern -f ../patterns/openingTonic $1 | head -1 | awk '{print $6}')
-opening_tonic2=$(pattern -f ../patterns/openingTonic $1 | tail -1 | awk '{print $9}')
+opening_tonic1=$(pattern -f ../patterns/openingTonic /tmp/$1.subbedOpening | head -1 | awk '{print $6}')
+opening_tonic2=$(pattern -f ../patterns/openingTonic /tmp/$1.subbedOpening | tail -1 | awk '{print $9}')
 awk -v opening_tonic=$opening_tonic1 -v opening_tonic2=$opening_tonic2 '{ if (!NF) print $0
 else if (NR > opening_tonic1 && NR == opening_tonic2) 
 print $0,"\011""T"
-else print $0}' $1  > /tmp/$1.tonicOpening
+else print $0}' /tmp/$1.subbedOpening  > /tmp/$1.tonicOpening
 #
 # #### Rule 2: Label closing I chords as tonic (beginning with first appearance of final tonicchord)
 closing_tonic_start=$(pattern -f ../patterns/closingTonic /tmp/$1.tonicOpening | head -1 | awk '{print $6}')
@@ -28,8 +35,8 @@ print $0,"\011""D (cadential 64)"
 else if (NR==second)
 	print $0,"\011""D (cadential 64)"
 else print $0}' /tmp/$1.tonicClosing > /tmp/$1.cad64
-
-### Diminished  as Dominant (first fule of V chord section)
+#
+# ### Diminished  as Dominant (first fule of V chord section)
 awk '{ if ($1 ~ /^.*viio/)
  print $1,"\011"$2,"\011""D (vii as dom.)"
 else print $0 }' /tmp/$1.cad64 > /tmp/$1.dominantsubs
@@ -48,20 +55,22 @@ awk -v texpand1=$texpand1 -v texpand2=$texpand2 '{ if (NR >= texpand1 && NR <= t
 else
 	print $0}' /tmp/$1.dominantsubs_vii > /tmp/$1.tonicExpansion
 
-####Tonic Expansion through vii.	
+####Tonic Expansion through vii.
 texpand_vii_1=$(pattern -f ../patterns/tonicDominant_vii_Expansion /tmp/$1.tonicExpansion | awk '{print $6}')
 texpand_vii_2=$(pattern -f ../patterns/tonicDominant_vii_Expansion /tmp/$1.tonicExpansion | awk '{print $9}')
 awk -v texpand_vii_1=$texpand1 -v texpand_vii_2=$texpand2 '{ if (NR >= texpand1 && NR <= texpand2)
 	print $1,"\011"$2,"\011""T (Tonic Expansion through vii)"
 else
 	print $0}' /tmp/$1.tonicExpansion > /tmp/$1.tonicExpansion_vii
-	
-####Cleaner
-sed 's/	 	/	/g' /tmp/$1.tonicExpansion_vii | sed 's/		/	/g' | 
-awk '{if($2 ~/\=/) print $1"\011"$2; else if ($2 ~/\*-/) print $1"\011"$2; print $0}' | grep -v "==	T" | grep -v "\*-	T"
 
-# awk 'BEGIN{FS="\t"}{if ($1 ~ /^[0-9]/)
-# print $1,"\011"$2,"\011"$3
-# else if ($1 ~ /^\=/)
-# print $1,"\011"$2,"\011"$3
-# else print $0}
+###Root Position Tonics####
+awk '{if ($1 ~ /[0-9]\.I/ && $3 == "") print $1"\011"$2"\011""T"; else if ($1 ~ /[0-9]I/ && $3 == "") print $1"\011"$2"\011""T";
+ else print $0}' /tmp/$1.tonicExpansion_vii > /tmp/$1.forCleaning
+
+####Cleaner
+sed 's/	 	/	/g' /tmp/$1.forCleaning | sed 's/		/	/g' |
+awk '{if($1 ~ /^\=/) print $1"\011"$2; else if ($1 ~ /^\*\-/) print $1"\011"$2; else print $0}' | 
+awk '{if ($1 ~ /\!/) print $1"\011"$2; 
+else if ($1 ~ /\*/) print $1"\011"$2;else print $0}' # > /tmp/$1.cleaned
+
+
