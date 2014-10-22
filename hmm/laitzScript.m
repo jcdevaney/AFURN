f0a76297b8fbf21e56cca90431ba50e88d16cf37
Johanna Devaney <jcdevaney@Johannas-MacBook-Air.local>
2014-10-21 23:16:59 -0400

    folder clean up

--- hmm/laitzScript.m
+++ hmm/laitzScript.m
@@ -1,10 +1,12 @@
 ruleDir='../ruleFilesTextBook/';
+ruleDir2='../ruleFilesTextBookPME/';
 textDir='../textBook/';
 wbDir='../workbook';
 
 ignore=[33 52 53 56  58  59 67 70 103  114  117];
 maxFiles=128;
 [rules, pred]=readRuleFiles(ruleDir,ignore,maxFiles);
+[rules2, pred2]=readRuleFiles(ruleDir2,ignore,maxFiles);
 
 hmm=PRstruct;
 
